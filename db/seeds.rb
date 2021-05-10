6cb4f9167a35ce7cc0fbd4573a212f988bcce116
mayagbarnes <maya8617@gmail.com>
2021-05-09 23:19:34 -0700

    Add seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -12,7 +12,6 @@ Transaction.delete_all
 Investment.delete_all
 
 
-
 demo_login = User.create( {username: 'DemoLogin', password: '123456'})
 account_1 = Account.create( { account_name: 'Checking', institution: 'Citi', category: 'Cash', balance: 1000.77, user_id: demo_login.id} )
 account_2 = Account.create( { account_name: 'Savings', institution: 'Bank of America', category: 'Cash', balance: 4000.50, user_id: demo_login.id } )
@@ -21,25 +20,39 @@ account_4 = Account.create( { account_name: 'Credit Card', institution: 'Chase',
 account_5 = Account.create( { account_name: 'Individual', institution: 'Charles Schwab', category: 'Investment', balance: 1500.50, user_id: demo_login.id } )
 account_6 = Account.create( { account_name: 'Roth IRA', institution: 'Charles Schwab', category: 'Investment', balance: 7000.30, user_id: demo_login.id } )
 
-# account1 - transactions
+# checking account transactions
 
-transaction_1 = Transaction.create( { description: 'Cox Internet', category: 'Bills and Utilities', date: "2021-04-09", amount: -70.00 , account_id: account_1.id} )
-transaction_2 = Transaction.create( { description: 'Wendys', category: 'Food and Dining', date: "2021-04-09", amount: -20.50, account_id: account_1.id } )
-transaction_3 = Transaction.create( { description: 'Amazon', category: 'Shopping', date: "2021-04-07", amount:  -40.25, account_id: account_1.id } )
-transaction_4 = Transaction.create( { description: 'Credit Card Payment', category: 'Bills and Utilities', date: "2021-04-06", amount:  -300.50, account_id: account_1.id } )
-transaction_5 = Transaction.create( { description: 'Paycheck', category: 'Income', date: "2021-04-06", amount: 2500.50, account_id: account_1.id } )
-transaction_6 = Transaction.create( { description: 'Gym Membership', category: 'Health and Fitness', date: "2021-04-05", amount: -75.00, account_id: account_1.id } )
+transaction_1 = Transaction.create( { description: 'Paycheck', category: 'Income', date: "2021-04-15", amount: 2000.50, account_id: account_1.id } )
+transaction_2 = Transaction.create( { description: 'Paycheck', category: 'Income', date: "2021-04-30", amount: 2000.50, account_id: account_1.id } )
+transaction_3 = Transaction.create( { description: 'Mortgage Payment', category: 'Home', date: "2021-04-02", amount:  -1750.25, account_id: account_1.id } )
+transaction_4 = Transaction.create( { description: 'Cox Internet', category: 'Bills and Utilities', date: "2021-04-05", amount: -70.00 , account_id: account_1.id} )
+transaction_5 = Transaction.create( { description: 'Water Bill', category: 'Bills and Utilities', date: "2021-04-17", amount: -75.39 , account_id: account_1.id} )
+transaction_6 = Transaction.create( { description: 'Energy Bill', category: 'Bills and Utilities', date: "2021-04-09", amount: -110.76 , account_id: account_1.id} )
+transaction_7 = Transaction.create( { description: 'Trash Bill', category: 'Bills and Utilities', date: "2021-04-04", amount: -20.24 , account_id: account_1.id} )
+transaction_8 = Transaction.create( { description: 'Sewer', category: 'Bills and Utilities', date: "2021-04-04", amount: -21.99 , account_id: account_1.id} )
+transaction_9 = Transaction.create( { description: 'Gym Membership', category: 'Health and Fitness', date: "2021-04-20", amount: -85.99, account_id: account_1.id } )
+transaction_10 = Transaction.create( { description: 'Philz Coffee', category: 'Food and Dining', date: "2021-04-06", amount:  -7.50, account_id: account_1.id } )
+transaction_11 = Transaction.create( { description: 'Spotify', category: 'Entertainment', date: "2021-04-01", amount: -9.99, account_id: account_1.id } )
+transaction_12 = Transaction.create( { description: 'Netflix', category: 'Entertainment', date: "2021-04-01", amount: -14, account_id: account_1.id } )
+transaction_13 = Transaction.create( { description: 'Sephora', category: 'Personal Care', date: "2021-04-26", amount: -150.47, account_id: account_1.id } )
+transaction_14 = Transaction.create( { description: 'Amazon', category: 'Shopping', date: "2021-04-23", amount: -40.56, account_id: account_1.id } )
+transaction_15 = Transaction.create( { description: 'Amazon', category: 'Shopping', date: "2021-04-07", amount: -71.25, account_id: account_1.id } )
+transaction_16 = Transaction.create( { description: 'Chilis', category: 'Food and Dining', date: "2021-04-06", amount: -23.12, account_id: account_1.id } )
+transaction_17 = Transaction.create( { description: 'Yukga KBBQ', category: 'Food and Dining', date: "2021-04-24", amount: -65.38, account_id: account_1.id } )
+transaction_18 = Transaction.create( { description: 'Wendys', category: 'Food and Dining', date: "2021-04-18", amount: -16.84, account_id: account_1.id } )
+transaction_19 = Transaction.create( { description: 'Chipotle', category: 'Food and Dining', date: "2021-04-12", amount: -19.29, account_id: account_1.id } )
+transaction_20 = Transaction.create( { description: 'Philz Coffee', category: 'Food and Dining', date: "2021-04-29", amount: -7.82, account_id: account_1.id } )
 
-# account1 - investments
 
-investment_1 = Investment.create( { inv_name: 'Apple', ticker: 'AAPL', shares: 100.00, prev_close: 0, price_paid: 100.00, account_id: account_5.id, last_fetch: "2021-05-01" } )
-investment_2 = Investment.create( { inv_name: 'Microsoft', ticker: 'MSFT', shares: 50.00, prev_close: 0, price_paid: 200.00, account_id: account_5.id, last_fetch: "2021-05-01"} )
+# individual brokerage investments
+
+investment_1 = Investment.create( { inv_name: 'Apple', ticker: 'AAPL', shares: 20.00, prev_close: 0, price_paid: 100.00, account_id: account_5.id, last_fetch: "2021-05-01" } )
+investment_2 = Investment.create( { inv_name: 'Microsoft', ticker: 'MSFT', shares: 10.00, prev_close: 0, price_paid: 200.00, account_id: account_5.id, last_fetch: "2021-05-01"} )
 investment_3 = Investment.create( { inv_name: 'Google', ticker: 'GOOGL', shares: 1.00, prev_close: 0, price_paid: 2000.00, account_id: account_5.id, last_fetch: "2021-05-01"} )
+investment_4 = Investment.create( { inv_name: 'Zendesk', ticker: 'ZEN', shares: 25.00, prev_close: 0, price_paid: 95.00, account_id: account_5.id, last_fetch: "2021-05-01"} )
 
+# Roth investments
 
-# transaction = { description: 'yikes', category: 'Health and Fitness', date: "2021-10-31", amount: -75.00, account_id: 26 }
-# $.ajax({
-#         method: 'GET',
-#         url: '/api/transactions/search',
-#         data: {query: 'pay'}
-# })
+investment_5 = Investment.create( { inv_name: 'Russell 3000 ETF', ticker: 'SPY', shares: 47.00, prev_close: 0, price_paid: 225.00, account_id: account_6.id, last_fetch: "2021-05-01" } )
+investment_6 = Investment.create( { inv_name: 'MSCI ACWI ETF', ticker: 'ACWI', shares: 40.00, prev_close: 0, price_paid: 88.00, account_id: account_6.id, last_fetch: "2021-05-01"} )
+investment_7 = Investment.create( { inv_name: 'Vanguard Bond ETF', ticker: 'BND', shares: 75.00, prev_close: 0, price_paid: 80.00, account_id: account_6.id, last_fetch: "2021-05-01"} )
