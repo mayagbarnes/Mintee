6da0e25dae64a50cc32712f814d5d2a39b247961
mayagbarnes <maya8617@gmail.com>
2021-05-22 23:47:45 -0700

    Update sidebar account item to functional

--- frontend/components/sidebar/sidebar_account_item.jsx
+++ frontend/components/sidebar/sidebar_account_item.jsx
@@ -1,28 +1,24 @@
 import React from 'react';
 
-class SidebarAccountItem extends React.Component {
-    constructor(props) {
-        super(props);
-    }
+const SidebarAccountItem = (props) => {
+    let balance = parseFloat(props.account.balance);
+    balance.toFixed(2);
 
-    render() {
-        let balance = parseFloat(this.props.account.balance);
-            balance.toFixed(2);
-          var formatter = new Intl.NumberFormat('en-US', {
-                style: 'currency',
-                currency: 'USD',
-            });
-        
-        return (
-            <div>
-                <div className='sidebar-account-primary'>
-                    <h4 className='account-name' >{this.props.account.account_name}</h4>
-                    <h4 className='sidebar-account-balance'>{formatter.format(balance)}</h4>
-                </div>
-                <p className='sidebar-account-institution'>{this.props.account.institution}</p>
+    var formatter = new Intl.NumberFormat('en-US', {
+        style: 'currency',
+        currency: 'USD',
+    });
+
+    return (
+        <div>
+            <div className='sidebar-account-primary'>
+                <h4 className='account-name' >{props.account.account_name}</h4>
+                <h4 className='sidebar-account-balance'>{formatter.format(balance)}</h4>
             </div>
-        )
-    }
+            <p className='sidebar-account-institution'>{props.account.institution}</p>
+        </div>
+    )
+
 }
 
 export default SidebarAccountItem;
